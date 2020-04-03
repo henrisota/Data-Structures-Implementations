e27d834f887863c231f2f369c1386cac9237791a
henrisota <henrisota@users.noreply.github.com>
2020-04-03 13:25:05 +0200

    Constructor and Basic Logic of String Hash
    Created constructor of Hash and defined it. Wrote down a simple algorithm to calculate a hash based on the sum of the ASCII values of the characters of the string.

--- Hash Table/src/Hash.cpp
+++ Hash Table/src/Hash.cpp
@@ -4,11 +4,18 @@
 
 #include "Hash.h"
 
+Hash::Hash() {
+    this->tableSize = 20;
+}
+
 int Hash::hash(const std::string &key) {
     int hash = 0;
     int index;
 
-    index = key.length();
+    for (int i = 0; i < key.length(); i++)
+        hash += (int) key[i];
 
-    return index;
+    index = hash % this->tableSize;
+
+    return hash;
 }
--- Hash Table/src/Hash.h
+++ Hash Table/src/Hash.h
@@ -6,7 +6,10 @@
 #define HASH_TABLE_HASH_H
 
 class Hash {
+    private:
+        int tableSize;
     public:
+        Hash();
         int hash(const std::string &key);
 };
 
